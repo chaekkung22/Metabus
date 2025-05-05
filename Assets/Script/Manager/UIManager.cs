9eae4a3472259abb07872201a5140f2adeaab671
chae_kkung <ycy0109@naver.com>
2025-05-05 22:04:33 +0900

    add : TMP추가

--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
-{
-    
+{  
+    public void ChangeToFlappyPlaneScene()
+    {
+        SceneManager.LoadScene("FlappyPlaneScene");
+    }
+
+    public void ChangeToTheStackScene()
+    {
+        SceneManager.LoadScene("TheStackScene");
+    }
 }
