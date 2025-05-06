a762e3d431e6227709d5a09e4323bb81a860d46d
chae_kkung <ycy0109@naver.com>
2025-05-07 04:14:07 +0900

    feat : 카메라가 플레이어를 따라가는 기능 추가

--- Assets/Script/FlappyPlaneScene/FollowCamera.cs
+++ Assets/Script/FlappyPlaneScene/FollowCamera.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] bool useClamp;
+    [SerializeField] float minX, maxX, minY, maxY;
     float offsetX;
+    float offsetY;
     private void Start()
     {
-       offsetX =  transform.position.x - target.position.x;
+        offsetX = transform.position.x - target.position.x;
+        if(useClamp )
+        offsetY = transform.position.y - target.position.y;
     }
 
     private void Update()
     {
         Vector3 pos = transform.position;
         pos.x = target.position.x + offsetX;
+
+
+        if (useClamp)
+        {
+            pos.y = target.position.y + offsetY;
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        }
+
         transform.position = pos;
     }
 }
